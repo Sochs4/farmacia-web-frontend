f89a00ee3f1bb93657936aab5c45e37290e0ec1c
Sergio Och <sochs@miumg.edu.gt>
2026-05-28 10:16:32 -0600

    Actualizar API HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
- baseURL: "http://fart24.runasp.net/api"
+export default axios.create({
+  baseURL: "https://fart24.runasp.net/api",
 });
 
 export default api;
